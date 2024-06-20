01c9ee3a1fbddec062c3260e7d1ae51e71847bd8
Aycan <aycan.biilge@gmail.com>
2024-06-20 02:55:04 -0400

    added to userINterface

--- src/main/java/com/ps/UserInterface.java
+++ src/main/java/com/ps/UserInterface.java
@@ -12,18 +12,44 @@ import java.util.List;
 import java.util.Scanner;
 
 public class UserInterface {
+
+    private static LeaseDAO leaseDAO;
     private static VehicleDAO vehicleDAO;
     private static SalesDAO salesDAO;
-    private static LeaseDAO leaseDAO;
     private static Scanner scanner = new Scanner(System.in);
 
     public static void init(String[] args) {
+
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl("jdbc:mysql://localhost:3306/dealership_db");
         basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         basicDataSource.setUsername(args[0]);
         basicDataSource.setPassword(args[1]);
 
+        leaseDAO = new LeaseDAO(basicDataSource);
+        salesDAO = new SalesDAO(basicDataSource);
         vehicleDAO = new VehicleDAO(basicDataSource);
+
     }
+
+    public static void display(String[] args) {
+
+        init(args);
+    }
+
+    private static void processSearchVehicles(){
+
+    }
+
+    private static void processAddVehicles(){
+
+    }
+
+    private static void prccessManageSales(){
+
+    }
+
+
+
+
 }
